f3d9fd1b576b4d881b62a0a38deee6f1da2da1b9
GabrielFeliciano <gfp001@outlook.com>
2023-01-26 09:07:46 -0300

    integrate

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as vaultcore from 'dotenv-vault-core';
 
 async function bootstrap() {
+  vaultcore.config();
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
